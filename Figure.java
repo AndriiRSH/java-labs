f1f40b45bccbe5be83efbc1bf8b8d5b7a5d1eadb
Андрій Рошило <102091946+AndriiRSH@users.noreply.github.com>
2022-12-12 01:45:29 +0200

    Add files via upload

---
+++ Figure.java
@@ -0,0 +1,6 @@
+package Models;
+
+public abstract class Figure {
+    public abstract double getArea();
+    public abstract String getName();
+}
---
+++ Main.java
@@ -0,0 +1,21 @@
+package Models;
+
+import Models.Serialize.JsonS;
+import Models.Serialize.TxtS;
+import Models.Serialize.XmlS;
+
+public class Main {
+
+
+//    XmlS CircleXmlS = new XmlS();
+//    CircleXmlS.writeObject();
+//
+//    JsonS figureJsonS = new JsonS();
+//    figureJsonS.writeObject();
+//
+//    var txtSerialize = new TxtS<Figure>();
+//    txtSerialize.writeObject();
+//    var res = txtSerialize.readObject();
+//
+//    System.out.println(res);
+}
