154fce1ee54d56aa48448f199da1c2fbdcdf9977
Андрій Рошило <102091946+AndriiRSH@users.noreply.github.com>
2022-12-11 21:22:09 +0200

    Update ComparatorCircle.java

--- ComparatorCircle.java
+++ ComparatorCircle.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class ComparatorCircle implements Comparator<Circle> {
     @Override
     public int compare(Circle o1, Circle o2){
-        return o1.getName().compareTo((o2.getName()));
+        return o1.getName().compareTo((o2.getRadius()));
     }
 }
